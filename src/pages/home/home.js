3cbf453b012116d8404b38bfed8d8a8c15180887
Jeet Shah <jeetshah4321@gmail.com>
2018-09-08 10:58:46 -0400

    Created home page, added styles, background image

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,7 +6,14 @@ class Home extends Component{
     render(){
         return(
             <div className="home-container">
-                <h2>Main Page</h2>
+                <img className="home-background" src="night_sky.jpeg" alt=""/>
+                <div className="home-content">
+                    <h1 className="home-title">&lt;Developer/&gt;</h1>
+                    <div>
+                        <button className="home-button">Resume</button>
+                        <button className="home-button">Contact Me</button>
+                    </div>
+                </div>
             </div>
         )
     }
