736f2b5378eeb5d683f6d2c17534efeba8d586c5
Jeet Shah <jeetshah4321@gmail.com>
2018-09-10 19:35:18 -0400

    Added europe article placeholder text and domain path

--- src/articles/europe-trip.js
+++ src/articles/europe-trip.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Article from '../partials/article'
+import PhotoCap from '../partials/photo-cap'
 
 const EuropeTrip = () => {
     return(
         <Article title="Reflection on Europe" author="Jeet Shah" date="08/19/2018">
-            <p>Text for Europe Trip</p>
+            <PhotoCap image="europe_group.jpg" caption="My friends and I at a cafe in Limburg, Germany"/>
+            <p>I had the chance to visit Europe from August 5th to August 18th and traveled to Ireland, France, Italy, and Germany!
+            In the next few days I will be writing up a reflection on my trip!</p>
         </Article>
     )
 }
